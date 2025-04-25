1e3355fa2585c809a13bfafaa02dce72e25d38a9
MR. Morningstar <ee8991471@gmail.com>
2025-04-25 15:00:51 +0600

    update

--- System.cpp
+++ System.cpp
@@ -1,78 +1,99 @@
 #include<iostream>
 using namespace std;
 
-
-
-struct tokenGenerator
-{
-    int travel_visa = 0;
-    int med_visa = 0;
-    int bus_visa = 0;
-    int gov_visa= 0;
-
-
-
+struct Token {
+    string name;
+    string visa_type;
 };
-
 
 int main()
 {
-    int exitORrun;
-    tokenGenerator t;
+    Token t[25];
+    int total = 0;
+    char choice;
+    string name;
 
-    cout<<"\t\tWelcome to  Embassy Consulate Service system"<<endl;
+    cout << "\t\tWelcome to Embassy Consulate Service System" << endl;
 
+    while(true) {
+        cout<<"\n\tPlease select one.." << endl;
+        cout << "\t1. Apply for Visa" << endl;
+        cout << "\t2. Search by Token Number" << endl;
+        cout << "\t3. Exit" << endl;
+        cout << "\t-------------------------------------------\n";
+        cout << "\tEnter your choice (1-3): ";
+        cin >> choice;
 
-    while(exitORrun !='q'){
+        if (choice == '3') {
+            cout << "\n\tThank you for using the system.\n";
+            break;}
 
-
-
-        cout<<"\tPlease select one.."<<endl<<endl;
-        cout<<"\t1.Travel Visa (TR)"<<endl;
-        cout<<"\t2.Medical Visa (MED)"<<endl;
-        cout<<"\t3.Business Visa (BS) "<<endl;
-        cout<<"\t4.Government Officials Visa (GO) "<<endl;
-        cout<<"\t-------------------------------------------\n\n";
-
-
-
-        int user;
-        cout<<">>> ";cin>>user;
-        if(user == 1)
-        {
-            t.travel_visa++;
-            if(t.travel_visa<=25){
-
-                    cout<<"----------------------------------------\n\n";
-                    cout<<"|\tTravel Token issued.\n";
-                    cout<<"|\tToken no: TR-"<<t.travel_visa<<endl<<endl;
-                    cout<<"-----------------------------------------\n\n";
-
-            }else
-            {
-                cout<<"\tsorry, Quota full\n\n";
+        if (choice=='1') {
+            if (total>= 25) {
+                cout << "\n\tToken limit reached!" << endl;
+                continue;
             }
 
-        }
+            char visaChoice;
+            cout << "\n\tSelect Visa Type:" << endl;
+            cout << "\t1. Travel Visa (TR)" << endl;
+            cout << "\t2. Medical Visa (MED)" << endl;
+            cout << "\t3. Business Visa (BS)" << endl;
+            cout << "\t4. Government Officials Visa (GO)" << endl;
+            cout << "\tEnter your choice (1-4): ";
+            cin >> visaChoice;
 
-        if(user == 2)
-        {
-            t.med_visa++;
-            if(t.med_visa <=2){
-                cout<<"----------------------------------------\n\n";
-                cout<<"\tMedical Token issued.\n";
-                cout<<"\tToken no: MED-"<<t.med_visa<<endl<<endl;
-                cout<<"----------------------------------------\n\n";
+            cout << "\tEnter your name: ";
+            cin.ignore();
+            getline(cin, name);
+
+            t[total].name = name;
+
+            if (visaChoice == '1')
+                {
+                t[total].visa_type = "TR";
             }
-                else{ cout<<"\tSorry, Quota full.\n\n";}
+            else if (visaChoice == '2') {
+                t[total].visa_type = "MED";
+            }
+            else if (visaChoice == '3') {
+                t[total].visa_type = "BS";
+            }
+            else if (visaChoice == '4') {
+                t[total].visa_type = "GO";
+            }
+            else {
+                cout << "\tInvalid visa type!\n";
+                continue;
+            }
+
+
+            cout<<"\tToken generated for " << t[total].name
+                 << " [Visa Type: " << t[total].visa_type
+                 << "] | Token No: " << (total + 1) << endl;
+
+            total++;
         }
 
-        if(user == 3){cout<<"\tSorry Business visa quota is currently full\n\n";}
+        else if (choice == '2') {
+            int token_no;
+            cout << "\n\tEnter Token Number to search: ";
+            cin >> token_no;
 
-        if(user == 4){cout<<"\tSorry Gov. visa quota is currently full\n\n";}
+            if (token_no <=0) {
+                cout << "\tInvalid Token Number!\n";
+            } else {
+                int idx = token_no - 1;
+                cout << "\tToken No: " << token_no
+                     << "\n\tName: " << t[idx].name
+                     << "\n\tVisa Type: " << t[idx].visa_type << endl;
+            }
+        }
 
-
-
+        else {
+            cout << "\tInvalid option! Try again.\n";
+        }
     }
-}
 
+    return 0;
+}
